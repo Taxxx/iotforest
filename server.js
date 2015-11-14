ac84a68b4bb8553756424af3e6f3f5d3398b1397
Henrry Chuquimia <henrry.el.master@gmail.com>
2015-11-14 02:10:49 -0400

    passport

--- server.js
+++ server.js
@@ -2,22 +2,32 @@
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
+var passport = require('passport');
+var flash    = require('connect-flash');
 var port  	 = process.env.port || 3000; 				// set the port
-//var database = require('./config/database'); 			// load the database config
+var database = require('./config/database'); 			// load the database config
 var morgan   = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session      = require('express-session');
 var methodOverride = require('method-override');
 
 // configuration ===============================================================
-//mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
+// required for passport
+app.use(session({ secret: 'ilovelightpath' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
 require('./app/routes/home.js')(app);
